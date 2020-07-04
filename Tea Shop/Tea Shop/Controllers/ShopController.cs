eca29de4640539d40e8a1c8149ff5bd857e27458
pushpavarsha <pushpavarsha5@gmail.com>
2020-07-04 21:13:41 +0530

    RepositoryPat and Interface Addition

---
+++ Tea Shop/Tea Shop/Controllers/ShopController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tea_Shop.Interfaces;
+using Tea_Shop.Models;
+using Tea_Shop.Repository;
+namespace Tea_Shop.Controllers
+{
+    public class ShopController : Controller
+    {
+        ThinkBridgeEntities db = new ThinkBridgeEntities();
+       
+        // private IShop shop;
+        //ShopController(RepositoryPat repository)
+        //{
+        //    shop = repository;
+        //}
+        // GET: Shop
+        [HttpGet]
+        public ActionResult GetDetails()
+        {
+            RepositoryPat repository = new RepositoryPat();
+            IEnumerable<TeaShop> itemList = repository.GetDetails();
+                //db.TeaShops.ToList();
+            return View(itemList);
+        }
+
+        [HttpGet]
+        public ActionResult CreateItem()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateItem(HttpPostedFileBase file,TeaShop teaShop)
+        {
+            string filename = Path.GetFileName(file.FileName);
+            string _filename = DateTime.Now.ToString("yymmssff") + filename;
+            string fileextension = Path.GetExtension(file.FileName);
+            string path = Path.Combine(Server.MapPath("~/Images/"), _filename);
+            teaShop.ImageFile = "~/Images/" + _filename;
+
+            RepositoryPat repository = new RepositoryPat();
+            int success = repository.InsertDetails(teaShop);
+            file.SaveAs(path);
+            return RedirectToAction("GetDetails");
+        }
+
+        [HttpGet]
+        public ActionResult GetItemDetail(int id)
+        {
+            var item = db.TeaShops.Where(x => x.ItemId == id).FirstOrDefault();
+            return View(item);
+        }
+       
+    }
+}
---
+++ Tea Shop/Tea Shop/Repository/RepositoryPat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.ModelBinding;
+using Tea_Shop.Interfaces;
+using Tea_Shop.Models;
+
+namespace Tea_Shop.Repository
+{
+    public class RepositoryPat : IShop
+    {
+        public void DeleteDetails(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TeaShop> GetDetails()
+        {
+            IEnumerable<TeaShop> item = null;
+            using(var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44370/api/");
+                var response = client.GetAsync("TeaApi/SaleDetails");
+                response.Wait();
+                var result = response.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var readresponse=result.Content.ReadAsAsync<IEnumerable<TeaShop>>();
+                    readresponse.Wait();
+                    item = readresponse.Result;
+                }
+                else
+                {
+                    item = Enumerable.Empty<TeaShop>();
+            
+                }
+                return item;
+            }
+           
+        }
+
+        public int InsertDetails(TeaShop item)
+        {
+            using(var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44370/api/");
+                var postTask = client.PostAsJsonAsync<TeaShop>("TeaApi/AddItem/", item);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return 1;
+                }
+                else
+                    return 0;
+            }
+           
+        }
+
+        public int UpdateDetails(TeaShop item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
